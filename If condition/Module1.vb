8486a09f73f6662f58dff099c94599dfbc0af92d
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-11-16 14:15:50 +0200

    No Going back I Swear to God

---
+++ If condition/Module1.vb
@@ -0,0 +1,15 @@
+﻿
+
+
+
+Module Module1
+
+    Sub Main()
+
+        Public void
+
+
+
+    End Sub
+
+End Module
---
+++ If condition/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("If condition")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("rg-adguard")>
+<Assembly: AssemblyProduct("If condition")>
+<Assembly: AssemblyCopyright("Copyright © rg-adguard 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ef9965d2-9ccd-4555-ac46-2edc568148fd")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
